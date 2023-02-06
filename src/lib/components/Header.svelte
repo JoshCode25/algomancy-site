0aaa7066281ef99af65c7bc874adccd481883190
JoshG <joshcode25@gmail.com>
2023-02-06 06:34:12 -0600

    added logo sample

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,11 +1,18 @@
+<script>
+	import logo from '$lib/assets/algomancyLogo.png';
+</script>
+
 <header>
 	<h4>Days until Kickstarter Launch: ##</h4>
 </header>
 <nav>
-	<a href="/">HOME</a>
-	<a href="blog">BLOG</a>
-	<a href="/cardsearch">CARD SEARCH</a>
-	<a href="/about">ABOUT</a>
+	<img src={logo} alt="Algomancy Logo" />
+	<div class="flexContainer">
+		<a href="/">HOME</a>
+		<a href="blog">BLOG</a>
+		<a href="/cardsearch">CARD SEARCH</a>
+		<a href="/about">ABOUT</a>
+	</div>
 </nav>
 
 <style>
@@ -21,5 +28,12 @@
 	}
 	a {
 		margin: 10px;
+	}
+	img {
+		width: 100px;
+		height: 100px;
+	}
+	.flexContainer {
+		display: flex;
 	}
 </style>
