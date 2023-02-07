3ec2276b7c928779dceb18b592ace08e3e9edd45
JoshG <joshcode25@gmail.com>
2023-02-07 06:37:30 -0600

    worked on game summary wrapping

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,27 +10,33 @@
 
 <h1>Algomancy</h1>
 
-<div class="flexContainer">
-	<CardContainer cardInfo={displayCardInfo} />
+<div class="displayContainer">
+	<div class="imgWrapper">
+		<CardContainer cardInfo={displayCardInfo} />
+	</div>
 	<div class="summaryContainer">
 		<h2>Game Summary</h2>
 		<p>
-			This is a longer section with some descriptions about the game and what to
-			expect from it. There's likely two or three sentences here to really get
-			the point across, but not in a long wordy sort of way.
+			Algomancy is a complete card game, meaning you will get every card needed
+			to play the game all at once. No expensive singles, no problems with
+			scarcity, just excellent gameplay at an affordable price.
+		</p>
+		<p>
+			The game is both a fully balanced and ready to play 8 player game as well
+			as a card pool for a variety of constructed formats. So you can use it as
+			a board game to play with your friends and a source of cards to battle it
+			out in customizable constructed decks.
 		</p>
 	</div>
 </div>
 
 <style>
-	.flexContainer {
+	.displayContainer {
 		display: flex;
-		justify-content: space-around;
 		flex-wrap: wrap;
 		gap: 20px;
 	}
-
 	.summaryContainer {
-		max-width: 300px;
+		max-width: 600px;
 	}
 </style>
