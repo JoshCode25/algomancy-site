0aa284763f36d5636fa6475f73e51ba40a67613f
JoshG <joshcode25@gmail.com>
2023-02-10 06:38:45 -0600

    remaned faction color variablenames started dynamic styling

--- src/lib/components/SearchButton.svelte
+++ src/lib/components/SearchButton.svelte
@@ -1,42 +1,30 @@
 <script>
 	export let factionName;
 
-	let buttonClassOn = `button-${factionName}`;
-	let buttonClassOff = 'button-Off';
+	let buttonActive = true;
+	let button;
+	let activeButtonColor = `var(--faction-color-${factionName})`;
 
+	// $: if (buttonActive && button) {
+	// 	button.style.backgroundColor = `var(--faction-color-${factionName})`;
+	// }
 	const toggleFactionFilter = () => {
-		let toggledFaction = {};
-		toggledFaction[factionName] = !factionFilter[factionName];
-		setFactionFilter((factionFilter) => ({
-			...factionFilter,
-			...toggledFaction,
-		}));
+		buttonActive = !buttonActive;
+		console.log(buttonActive, button);
 	};
 </script>
 
-return (
 <button
 	type="button"
+	bind:this={button}
 	onClick={toggleFactionFilter}
-	className={`algomancy-cards-button ${
-		factionFilter[factionName] ? buttonClassOn : buttonClassOff
-	}`}>{factionName}</button
+	class:buttonActive>{factionName}</button
 >
-) }
 
 <style>
-	html {
-		--earth-brown: brown;
-		--wood-green: green;
-		--fire-red: red;
-		--water-blue: cornflowerblue;
-		--metal-gray: lightsteelblue;
-		--colorless-gray: gray;
-	}
-
-	div#algomancyButtonWrapper > button.algomancy-cards-button {
-		color: black;
-		background-color: whitesmoke;
+	button {
+		color: gray;
+		background-color: var(--faction-color-inactive);
 		font-size: 16px;
 		margin: 5px 5px;
 		min-width: 80px;
@@ -45,43 +33,13 @@ return (
 		border-radius: 8px;
 	}
 
-	div#algomancyButtonWrapper > button.algomancy-cards-button:hover {
+	button:hover {
 		cursor: pointer;
 		opacity: 0.8;
 	}
 
-	div#algomancyButtonWrapper > button.button-earth {
+	.buttonActive {
 		color: white;
-		background-color: var(--earth-brown);
-	}
-
-	div#algomancyButtonWrapper > button.button-wood {
-		color: white;
-		background-color: var(--wood-green);
-	}
-
-	div#algomancyButtonWrapper > button.button-fire {
-		color: white;
-		background-color: var(--fire-red);
-	}
-
-	div#algomancyButtonWrapper > button.button-water {
-		color: white;
-		background-color: var(--water-blue);
-	}
-
-	div#algomancyButtonWrapper > button.button-metal {
-		color: white;
-		background-color: var(--metal-gray);
-	}
-
-	div#algomancyButtonWrapper > button.button-colorless {
-		color: white;
-		background-color: var(--colorless-gray);
-	}
-
-	.button-off {
-		color: gray;
-		background-color: whitesmoke;
+		background-color: ;
 	}
 </style>
--- src/routes/cardsearch/+page.svelte
+++ src/routes/cardsearch/+page.svelte
@@ -1,3 +1,8 @@
+<script>
+	import SearchButton from '../../lib/components/SearchButton.svelte';
+</script>
+
 <h1>Search all available Algomancy cards</h1>
 
 <p>Card Search goes here</p>
+<SearchButton factionName={'earth'} />
