2f127410234aaafb9213adf6ccaae1cffea4ca64
JoshG <joshcode25@gmail.com>
2023-02-24 06:34:24 -0600

    added social media links to footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,9 +1,21 @@
 <footer>
 	<p>Algomancy design by Caleb Gannon 2023</p>
 	<div>
-		<p>Discord</p>
-		<p>Image Gallery</p>
-		<p>Social Media</p>
+		<a target="_blank" rel="noreferrer" href="https://discord.gg/EQyyjdf4Dr"
+			>Discord</a
+		>
+		<a
+			target="_blank"
+			rel="noreferrer"
+			href="https://instagram.com/caleb_gannon_official?igshid=ZDdkNTZiNTM="
+			>Instagram</a
+		>
+		<a
+			target="_blank"
+			rel="noreferrer"
+			href="https://twitter.com/CalebGannonMTG?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"
+			>Twitter</a
+		>
 	</div>
 </footer>
 
@@ -17,5 +29,15 @@
 	}
 	p {
 		margin: 20px;
+	}
+	a {
+		margin: 20px;
+		text-decoration: none;
+		color: var(--logo-black);
+		font-size: 16px;
+	}
+	a:hover {
+		color: var(--logo-tan);
+		cursor: pointer;
 	}
 </style>
